5e594d13a7a81484a93c3e20d8349a7cf92ae060
WillTRM <william.ravanesi@gmail.com>
2024-09-07 11:35:35 -0500

    hghjghj

--- dashboard/speedometer.py
+++ dashboard/speedometer.py
@@ -16,6 +16,8 @@ long_notch = 2.4
 short_notch = 2.2
 #how often notches should show up
 notch_interval = 25
+#you know what this does
+font_size = 30
 #speedometer range (lowest speed, highest speed)
 bounds = [0, 100]
 
@@ -35,46 +37,50 @@ class MyWindow(Gtk.Window):
         self.add(self.darea)
 
     def on_draw(self, wid, cr):
+        #font settings
+        cr.select_font_face("Courier", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+        cr.set_font_size(font_size)
+
         #draw outer circle
         cr.set_source_rgb(255, 255, 255)
         cr.arc((width / 2), (height / 2), (height / 2), 0, (2 * math.pi))
         cr.set_source_rgb(0, 0, 0)
         cr.fill_preserve()
         cr.set_source_rgb(255, 255, 255)
-
         cr.stroke()
 
         #draw inner circle
         cr.arc((width / 2), (height / 2), (height / 5), 0, (2 * math.pi))
-        
         cr.stroke()
 
-        #draw text
-        cr.select_font_face("Arial", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
-        cr.set_font_size(50)
-        x, y = get_coordinates_from_speed(40, bounds)
-        print(x, y)
-        #cr.move_to((x * width_center) + (width_center / 2), (y * -height_center) + (height_center / 2))
-        #cr.move_to((x * width_center) + width_center / 2, (y * -height_center) + height_center / 2)
-        cr.move_to(x * -100, y * 100)
-        cr.show_text("0")
-        cr.stroke()
-
-        #draw notches
+        #draw notches + notch text
         for notch_num, i in enumerate(linspace(bounds[0] - degree_offset, bounds[1] + degree_offset, num = notch_count)):
+            cr.set_source_rgb(255, 255, 255)
             x, y = get_coordinates_from_speed(i, bounds)
             cr.move_to((x * width_center) + width_center, (y * -height_center) + height_center)    # set start position along circle
             #set end position based on whether notch should be long
-            if (notch_num + 1) % notch_interval == 0 or notch_num == 0:    # long notch
+            if (notch_num + 1) % notch_interval == 0 or notch_num == 0:    # long notch + text
                 cr.set_line_width(3)
                 cr.line_to(x * (width / long_notch) + width_center, y * (height / -long_notch) + height_center)
+                cr.stroke()
+
+                cr.set_source_rgb(255, 0, 0)
+                cr.move_to(x * (width / long_notch) + width_center, y * (height / -long_notch) + height_center)
+                cr.show_text(f"{notch_num}")
+                cr.stroke()
             else:    # short notch
                 cr.set_line_width(1)
                 cr.line_to(x * (width / short_notch) + width_center, y * (height / -short_notch) + height_center)
-
-            cr.stroke()
+                cr.stroke()
+            
+        #draw speed text
+        cr.set_source_rgb(255, 0, 0)
+        #that three should be character count
+        #idk it doesn't center anyway lmao
+        cr.move_to(width_center - (3 * font_size / 4), height_center + (font_size / 4))
+        cr.show_text("420")
+        cr.stroke()
         
-
 #given speed and boundaries (lowest speed at angle pi and highest speed at angle 0), output speedometer coordinates
 #coordinates have a domain and range of [0, 1] so transformations can be easily applied later
 def get_coordinates_from_speed(speed: int, bounds: tuple[int, int]):
